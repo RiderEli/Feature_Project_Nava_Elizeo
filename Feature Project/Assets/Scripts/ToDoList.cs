efe8f94e964ce29a53bcba7114884e9b913b1528
Elizeo Nava <116670069+RiderEli@users.noreply.github.com>
2023-11-12 22:14:52 -0800

    Adding More Assets

---
+++ Feature Project/Assets/Scripts/ToDoList.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToDoList : MonoBehaviour
+{
+ /* TO DO LIST (Sangonomiya Kokomi's ability set from Genshin Impact)
+  * 1. Make the player controller by using the input system for movement and "Get Key" Functions for the abilities and the attacks.
+  * 2. Create Enemies by making a single class for demonstration on the skill set.
+  * 3. Create prefabs for the Attack's projectile and then add an instantiation script along with a code that will prevent rapid fire.
+  * 4. Create a Prefab for her E skill that allows her to summon a Jellyfish that heals the player in a certain radius. Apply effects for the healing effect.
+  * 5. Create a Script for her Q Ultimate skill by applying a buff to the player's healing and power and make her walk on water for a short period.
+  * 6. Create a body of water in the scene that will help demonstrate one of parts of her Ultimate.
+  */
+}
